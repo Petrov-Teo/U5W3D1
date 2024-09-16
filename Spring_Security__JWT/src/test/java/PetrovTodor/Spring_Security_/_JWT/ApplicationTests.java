68d03bf2cd45808bd8c160ca306b29a368c52888
Petrov-Teo <168548498+Petrov-Teo@users.noreply.github.com>
2024-09-16 15:13:50 +0200

    creato repo

---
+++ Spring_Security__JWT/src/test/java/PetrovTodor/Spring_Security_/_JWT/ApplicationTests.java
@@ -0,0 +1,13 @@
+package PetrovTodor.Spring_Security_._JWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
